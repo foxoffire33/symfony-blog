ddbd470fec786f348c2c6094af49d80d66ad1e19
Reinier de la Parra <reinier@vonq.com>
2022-09-15 16:03:39 +0200

    files.

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -34,7 +34,7 @@ class CategoryController extends AbstractController
             return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('category/new.html.twig', [
+        return $this->renderForm('admin/category/new.html.twig', [
             'category' => $category,
             'form' => $form,
         ]);
